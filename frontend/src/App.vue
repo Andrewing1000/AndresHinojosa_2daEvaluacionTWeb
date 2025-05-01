e115a2f308d8e307338dc4361e974c77f2af5974
AndresHinojosa <andrewing.fis@gmail.com>
2025-04-30 22:00:51 -0400

    INicio

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  
+  <!-- wrap the view in a flex child -->
+  <div class="app-content">
+      <Navbar>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </Navbar>
+    <router-view/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import Navbar from "./components/Navbar/Navbar.vue";
+</script>
+
+<style>
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100svh;
+  width: 100svw;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+#app {
+  display: flex;
+  flex-direction: column;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.app-content {
+  flex: 1;
+  overflow-y: auto;
+
+  background-image: url('/images/d7a61190a836bdcfc62bf97af4f4c74b.png');
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
---
+++ frontend/src/components/Navbar/Navbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav aria-label="Main navigation">
+    <ul class="navbar">
+      <slot />
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+// No se necesita lógica reactiva aquí, solo se expone el slot
+</script>
+
+<style scoped>
+
+nav {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background: transparent;
+  z-index: 1000;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-block: 2rem;
+  padding-top: 3rem;
+
+  height: 150px;
+  overflow: hidden;
+
+  pointer-events: visiblePainted;
+}
+
+.navbar {
+    display: flex;
+    gap: 1rem;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    
+    justify-content: space-evenly;
+    justify-items: center;
+    
+    height: 1500px;
+    width: 100%;
+    
+    transition: transform 1s ease 1s, opacity 4s ease 1s;
+    opacity: 0;
+    transform: translateY(-100%);
+    
+    list-style: none;
+}
+
+.navbar:hover{
+    transition: transform 1s ease 0s, opacity 4s ease 0s;
+    transform: translateY(0px);
+    opacity: 1;
+}
+
+.navbar *{
+    font-size: 20px;
+    font-family: "News Cycle";
+    font-weight: 100;
+    color: #FFE81F;
+}
+</style>
